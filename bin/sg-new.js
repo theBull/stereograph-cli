c8c299707818ee5ad15a4ddc8257c8a592dfb497
Danny Bullis <daniel.x.bullis.-nd@disney.com>
2018-01-24 10:00:56 -0800

    Publishing latest version to npm

--- bin/sg-new.js
+++ bin/sg-new.js
@@ -75,12 +75,7 @@ function copy_boilerplate_to_new(name, project_title, output_dir, debug, verbose
   verbose && console.log(`Copying boilerplate to ${output_dir}...`);
 
   // do not attempt to copy node_modules
-  fs.copy(`${__dirname}/boilerplate`, output_dir, {
-    filter: (path) => { 
-      verbose && console.log(`Checking path: ${path}`);
-      return path.indexOf('node_modules') < 0;
-    }
-  }, (err) => {
+  fs.copy(`${__dirname}/boilerplate`, output_dir, (err) => {
 
     if(err) {
       console.log('An error occurred while trying to copy the boilerplate project.');
